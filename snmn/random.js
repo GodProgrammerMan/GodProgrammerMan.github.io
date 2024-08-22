c3bce637050a314d93a2e232d71c9dbbe2d89134
lzx <960842214@qq.com>
2024-08-22 18:45:20 +0800

    Site updated: 2024-08-22 18:45:14

--- snmn/random.js
+++ snmn/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4a17b156.html"];function toRandomPost(){
+var posts=["posts/8c53d709.html","posts/63d6a691.html","posts/4a17b156.html","posts/9f3081ce.html","posts/9f3089ce.html","posts/b15d8e56.html","posts/9d0b4d7.html","posts/55dccea6.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
