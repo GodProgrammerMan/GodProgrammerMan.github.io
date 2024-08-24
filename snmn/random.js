5ad8940ac477f858387feffda1bc44f100bc3a01
lzx <960842214@qq.com>
2024-08-25 00:08:58 +0800

    Site updated: 2024-08-25 00:08:58

--- snmn/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["posts/63d6a691.html","posts/8c53d709.html","posts/4a17b156.html","posts/9f3081ce.html","posts/9f3089ce.html","posts/b15d8e56.html","posts/9d0b4d7.html","posts/55dccea6.html"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
